adc4df2e6562791db8dc46495c8d4ea6f25430c9
胡鹏 <hupenghp2005@gmail.com>
2019-06-26 21:28:43 +0800

    修改成现网配置

--- config/index.js
+++ config/index.js
@@ -18,7 +18,7 @@ module.exports = {
       //   }
       // }
       '/api': {    //将www.exaple.com印射为/apis
-          target: 'http://47.100.25.249:9080/jeefast/',  // 接口域名
+          target: 'http://47.100.25.249:8080/jeefast/',  // 接口域名
           secure: false,  // 如果是https接口，需要配置这个参数
           changeOrigin: true,  //是否跨域
           pathRewrite: {
