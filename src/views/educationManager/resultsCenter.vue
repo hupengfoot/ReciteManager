b2d7258ad741344b474c341f25ec538871d52072
胡鹏 <hupenghp2005@gmail.com>
2019-08-04 23:03:01 +0800

    修改resultcenter

--- src/views/educationManager/resultsCenter.vue
+++ src/views/educationManager/resultsCenter.vue
@@ -35,7 +35,8 @@
               {{scope.row.gender | gender}}
             </template>
           </el-table-column>
-          <el-table-column label="词汇量" prop="wordNum"></el-table-column>
+          <el-table-column label="已掌握的词汇量" prop="wordNum"></el-table-column>
+          <el-table-column label="已学到的词汇量" prop="learnWordNum"></el-table-column>
           <el-table-column label="进度" width="400">
             <template slot-scope="scope">
               <p class="progress"><span :style="{width:(scope.row.wordNum/maxWordNum)*300+'px'}"></span></p>
@@ -53,7 +54,8 @@
               {{scope.row.createTime}}
             </template>
           </el-table-column>
-          <el-table-column label="词汇量" prop="wordNum"></el-table-column>
+          <el-table-column label="已掌握的词汇量" prop="wordNum"></el-table-column>
+          <el-table-column label="已学到的词汇量" prop="learnWordNum"></el-table-column>
           <el-table-column label="进度" width="400">
             <template slot-scope="scope">
               <p class="progress groupProgress"><span :style="{width:(scope.row.wordNum/maxGroupWordNum)*300+'px'}"></span></p>
